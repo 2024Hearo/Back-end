8a6eaf240be28740dd5f14f59f9c40721afb27bd
knxxxn <20211308@sungshin.ac.kr>
2023-12-21 22:15:55 +0900

    스프링 기본

---
+++ Hearo/src/main/java/com/example/Hearo/HearoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hearo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearoApplication.class, args);
+	}
+
+}
---
+++ Hearo/src/test/java/com/example/Hearo/HearoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hearo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
