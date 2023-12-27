4d78dc7636c5c9166c185a2ba8f7371d2c44c90d
0gonge <challengef0802@gmail.com>
2023-12-27 18:30:29 +0900

    cloud연결

---
+++ Hear_o/src/main/java/com/hearos/hear_o/HearOApplication.java
@@ -0,0 +1,13 @@
+package com.hearos.hear_o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearOApplication.class, args);
+	}
+
+}
---
+++ Hear_o/src/test/java/com/hearos/hear_o/HearOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hearos.hear_o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
